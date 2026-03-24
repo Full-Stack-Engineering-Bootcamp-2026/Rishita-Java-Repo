915a55fecf6af799bcdab2b0f3a70383f74690b9
Rishita Pathak <rishita.pathak@mindbowser.com>
2026-03-24 16:46:28 +0530

    Day 02: Assignment 1

---
+++ Day02Asg01/springbootstructure/src/main/java/com/cdac/springbootstructure/SpringbootstructureApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.springbootstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstructureApplication.class, args);
+	}
+
+}
---
+++ Day02Asg01/springbootstructure/src/test/java/com/cdac/springbootstructure/SpringbootstructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.springbootstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
